d0f9a635f73eba726f285ed6f1b1d1d5072720af
chagasbezerra091-code <chagasbezerra091@gmail.com>
2025-10-28 17:08:08 -0300

    Create models.hpp

---
+++ bionic/models/models.hpp
@@ -0,0 +1,18 @@
+#ifndef MODELS_HPP
+#define MODELS_HPP
+struct testmodels {
+#define TestAptenOS /** o que testa o OS */
+#define TestAutomation /** API de Automação */
+#define TestApi
+}
+/**
+TestAptenOS
+           \
+            \
+             \
+            [Central API]
+                   |
+           [TestAutomation]
+                   |
+               [TestApi]
+*/
