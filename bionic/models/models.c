21b2e57ccaa5742af99b1dffc9c733d0305a2d0e
chagasbezerra091-code <chagasbezerra091@gmail.com>
2025-10-28 16:53:59 -0300

    Create models.c

---
+++ bionic/models/models.c
@@ -0,0 +1,11 @@
+/** copyright(c) chagas LLC */
+#include <cstdio.h> /** para printf */
+#include <models.hpp> /** header */
+/** a variavel AptenAllModels tem todos os modelos Apten
+* ela mostra os modelos se você chamar em um arquivo .cc
+  */
+void AptenAllModels() {
+printf("Apten all models:\n");
+printf("apten_one\n");
+printf("apten_two\n");
+}
